96299fee83e565792e556d3e0f6a1fcef7bd80c3
empucrs <edson.moreno@pucrs.br>
2023-09-05 12:23:08 -0300

    Esqueleto da lista estática

---
+++ Aula0905/listaEstatica/ListaEstatica.java
@@ -0,0 +1,45 @@
+/**
+ * ListaEstatica
+ */
+public class ListaEstatica {
+
+    boolean add(int value){
+        return false;
+    }
+
+    boolean add(int value, int index){
+        return false;
+    }
+    int get(int index){
+        return -1;
+    }
+    void remove(int index){
+    }
+    
+    int set(int value, int index){
+        return -1;
+    }
+    void clear(){
+    }
+
+    boolean contains(int value){
+        return false;
+    }
+    
+    int indexOf(int value){
+        return -1;
+    }
+    
+    boolean isEmpty(){
+        return false;
+    }
+    int lastIndexOf(int value){
+        return -1;
+    }
+    
+    int size(){
+        return 0;
+    }
+
+
+}
